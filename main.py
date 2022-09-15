d08089d472a29e635669a0a4ac4cc83e9ed2df60
Andrew Janedy <andrewjanedy@Andrews-MacBook.local>
2022-09-15 13:35:42 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# f-string
+
+your_name = input("What is your name?: ")
+num_credits = input("How many credits are you taking?: ")
+greeting = f"Hello {your_name}, I hope {num_credits} is just the right load"
+
+print("\n")
+print(greeting)
+print("\n")
+
+# arithmetic
+
+print(4+3)
+print(4-3)
+print(4*3)
+print(4/3)
+print(4//3) #floor division returns only the whole number and removes anything after the decimal
+print(4%3) #modulo returns the remainder from whole division
+
+# commenting multiple lines of code
+
+# You can comment out
+# multiple lines
+# at once
+# by toggling comments
+# with command+/
+
+# How to use git:
+# Go to VCS menu
+# Select "Create Git Repository..."
+# To access git, there should be a hidden file folder
+# within the project folder called .git
+
+
+
